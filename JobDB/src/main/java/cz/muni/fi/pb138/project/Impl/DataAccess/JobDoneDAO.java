58ee0b7857a163d9b9494cc9fcdbdcd0f59d8697
Sevitas <442365@mail.muni.cz>
2016-05-31 13:22:58 +0200

    DB added, waiting for impl.

--- JobDB/src/main/java/cz/muni/fi/pb138/project/Impl/DataAccess/JobDoneDAO.java
+++ JobDB/src/main/java/cz/muni/fi/pb138/project/Impl/DataAccess/JobDoneDAO.java
@@ -5,14 +5,88 @@ import cz.muni.fi.pb138.project.Exceptions.ServiceFailureException;
 import cz.muni.fi.pb138.project.Exceptions.ValidationException;
 import cz.muni.fi.pb138.project.Validators.JobDoneValidator;
 
+import java.io.File;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import org.exist.xmldb.XmldbURI;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.*;
+import org.xmldb.api.modules.CollectionManagementService;
+import org.xmldb.api.modules.XMLResource;
+import org.xmldb.api.modules.XQueryService;
+
 
 /**
  * Created by martin on 26.5.2016.
  */
 public class JobDoneDAO {
+
+    public final static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc";
+
+    private static final String fileName = "JobType.xml";
+
+    private static final String collection = "db/PB138";
+
+    private static String driver = "org.exist.xmldb.DatabaseImpl";
+
+    private XQueryService service;
+
+    protected static void usage() {
+        System.out.println("usage: org.exist.examples.xmldb.Put collection docName");
+        System.exit(0);
+    }
+
+    public static void main(String[] args) throws XMLDBException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        JobDoneDAO jobDoneDAO = new JobDoneDAO();
+        //jobDoneDAO.getAllJobDone();
+    }
+
+    public JobDoneDAO() throws XMLDBException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+
+
+        Class<?> cl = Class.forName(driver);
+        Database database = (Database)cl.newInstance();
+        DatabaseManager.registerDatabase(database);
+        Collection col = DatabaseManager.getCollection(URI + collection);
+
+        this.service = (XQueryService) col.getService("XQueryService", "1.0");
+        this.service.setProperty("indent", "yes");
+        System.out.println("connected");
+
+
+        /*
+        // initialize driver
+        Class<?> cl = Class.forName(driver);
+        Database database = (Database)cl.newInstance();
+        database.setProperty("create-database", "true");
+        DatabaseManager.registerDatabase(database);
+
+        // try to get collection
+        Collection col =
+                DatabaseManager.getCollection(URI + collection);
+        if(col == null) {
+            // collection does not exist: get root collection and create.
+            // for simplicity, we assume that the new collection is a
+            // direct child of the root collection, e.g. /db/test.
+            // the example will fail otherwise.
+            Collection root = DatabaseManager.getCollection(URI + XmldbURI.ROOT_COLLECTION);
+            CollectionManagementService mgtService =
+                    (CollectionManagementService)root.getService("CollectionManagementService", "1.0");
+            col = mgtService.createCollection(collection.substring((XmldbURI.ROOT_COLLECTION + "/").length()));
+        }
+        File f = new File(fileName);
+
+        // create new XMLResource
+        XMLResource document = (XMLResource)col.createResource(f.getName(), "XMLResource");
+
+        document.setContent(f);
+        System.out.print("storing document " + document.getId() + "...");
+        col.storeResource(document);
+        System.out.println("ok.");
+        */
+    }
 
     public void createJobDone(JobDone jobDone){
         try {
@@ -63,7 +137,17 @@ public class JobDoneDAO {
     }
 
     public List<JobDone> getAllJobDone(){
-        throw new UnsupportedOperationException();
+        String xquery = "for $x in /JobsDone/JobDone/userId/text() return $x ";
+        try {
+            ResourceSet result = service.query(xquery);
+            ResourceIterator iterator = result.getIterator();
+            while (iterator.hasMoreResources()){
+                System.out.println(iterator.nextResource().getContent().toString());
+            }
+        } catch (XMLDBException e) {
+            throw new ServiceFailureException("Error getting all JobDone.", e);
+        }
+        return null;
     }
 
     public List<JobDone> getAllJobDoneByUserId(long id){
