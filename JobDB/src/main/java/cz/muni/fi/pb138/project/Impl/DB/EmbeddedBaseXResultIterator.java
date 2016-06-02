5e420f78bc3dff5c1c7df373622fdc31b91b205f
xtomasch <422516@mail.muni.cz>
2016-06-02 11:56:06 +0200

    - forgotten 1 file

---
+++ JobDB/src/main/java/cz/muni/fi/pb138/project/Impl/DB/EmbeddedBaseXResultIterator.java
@@ -0,0 +1,89 @@
+package cz.muni.fi.pb138.project.Impl.DB;
+
+import org.basex.core.Context;
+import org.basex.core.cmd.Open;
+import org.basex.io.serial.Serializer;
+import org.basex.query.QueryException;
+import org.basex.query.QueryProcessor;
+import org.basex.query.iter.Iter;
+import org.basex.query.value.item.Item;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+/**
+ * Created by xtomasch on 6/2/16.
+ */
+public class EmbeddedBaseXResultIterator implements Iterator, AutoCloseable{
+    private QueryProcessor proc;
+    private Iter iter;
+    private Serializer ser;
+    private ByteArrayOutputStream stream;
+    private Item current;
+    private boolean hasNext;
+    private Context context;
+
+    public EmbeddedBaseXResultIterator(String query, String collection)throws QueryException, IOException{
+        context = new Context();
+
+        new Open(collection).execute(context);
+        QueryProcessor proc = new QueryProcessor(query, context);
+
+            // Store the pointer to the result in an iterator:
+            iter = proc.iter();
+            stream = new ByteArrayOutputStream();
+            // Create a serializer instance
+            ser = proc.getSerializer(stream);
+
+        current = iter.next();
+        if (current == null){
+            hasNext = false;
+        }else{
+            hasNext = true;
+        }
+    }
+
+    @Override
+    public String next() {
+        try{
+            stream.reset();
+            ser.serialize(current);
+            current = iter.next();
+            if (current == null){
+                hasNext = false;
+            }else{
+                hasNext = true;
+            }
+            return stream.toString();
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return hasNext;
+    }
+
+    @Override
+    public void forEachRemaining(Consumer action) {
+        while(this.hasNext){
+            action.accept(this.next());
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (proc != null){
+            proc.close();
+        }
+        if (ser != null){
+            ser.close();
+        }
+        if (context != null){
+            context.close();
+        }
+    }
+}
